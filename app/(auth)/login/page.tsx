335059ad627aa9d0348c23caf64897ec2d0dc321
Ian Yuson <ianjayyuson35@gmail.com>
2024-09-06 23:13:31 +0800

    API Supabase

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { LoginDesign } from "./component/LoginDesign";
+
+const LoginPage = () => {
+  return (
+    <div className="flex h-svh items-center">
+      <LoginDesign />
+    </div>
+  );
+};
+
+export default LoginPage;
---
+++ app/(auth)/signup/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { SignupDesign } from "./component/SignupDesign";
+
+const SignupPage = () => {
+  return (
+    <div className="flex h-svh items-center">
+      <SignupDesign />
+    </div>
+  );
+};
+
+export default SignupPage;
